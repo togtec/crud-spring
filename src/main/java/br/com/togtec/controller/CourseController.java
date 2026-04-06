74ada52928ed5dbbe5e9bf157e0892e23bb4f6d7
togtec <togtec.dev@gmail.com>
2026-04-06 15:14:41 -0300

    feat: implement course delete functionality

--- src/main/java/br/com/togtec/controller/CourseController.java
+++ src/main/java/br/com/togtec/controller/CourseController.java
@@ -46,4 +46,14 @@ public class CourseController {
                 .orElse(ResponseEntity.notFound().build());
     }
 
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        return repository.findById(id)
+                .map(recordFound -> {
+                    repository.deleteById(id);
+                    return ResponseEntity.noContent().<Void>build();
+                })
+                .orElse(ResponseEntity.notFound().build());
+    }
+
 }
