d595ba8dde1e19a54a2fdafc43e16aefb7b07a51
togtec <togtec.dev@gmail.com>
2026-05-12 21:28:53 -0300

    feat: add soft delete to Course

--- src/main/java/br/com/togtec/model/Course.java
+++ src/main/java/br/com/togtec/model/Course.java
@@ -6,10 +6,15 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
+import org.hibernate.annotations.Where;
 import org.hibernate.validator.constraints.Length;
 
 @Data
 @Entity
+@SQLDelete(sql = "UPDATE course SET status = 'Inactive' WHERE id = ?")
+@SQLRestriction("status = 'Active'")
 public class Course {
 
     @Id
@@ -28,4 +33,10 @@ public class Course {
     @Pattern(regexp = "Back-End|Front-End")
     @Column(length = 10, nullable = false)
     private String category;
+
+    @NotNull
+    @Length(max = 10)
+    @Pattern(regexp = "Active|Inactive")
+    @Column(length = 10, nullable = false)
+    private String status = "Active";
 }
